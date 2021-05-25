4dbd2ebf7f447fb13ef761533e3de3f209ab62ae
sanketshahc <sanket.shah.c@gmail.com>
2021-05-24 21:36:08 -0400

    all go, print statements kept

--- dev.py
+++ dev.py
@@ -1,9 +1,10 @@
 import hashtable_chaining as hc
-
+import hashtable_linear_probing as hcl
 
 table = hc.HashTable()
 data = hc.dummy_dat()
 vocab = list()
+
 for i, x in enumerate(data):
     if x not in vocab:
         vocab.insert(0,x)
@@ -16,3 +17,19 @@ for each in vocab:
     print(f'should be equal to {each}')
     print('\n')
 
+
+table = hcl.HashTable()
+data = hc.dummy_dat()
+vocab = list()
+for i, x in enumerate(data):
+    if x not in vocab:
+        vocab.insert(0,x)
+    if i > 100:
+        break
+    table.insert(x[0], x[1])
+
+
+for each in vocab:
+    print(each[0], table.get(each[0]))
+    print(f'should be equal to {each}')
+    print('\n')
